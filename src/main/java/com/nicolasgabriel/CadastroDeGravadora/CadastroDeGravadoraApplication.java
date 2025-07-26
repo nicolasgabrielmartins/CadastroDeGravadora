7e5e92ce763f3578ab877e042280822661d3cf5d
Nicolas Gabriel Martins <nicolasgabrielmartinssilva@gmail.com>
2025-07-25 22:16:19 -0300

    first commit

---
+++ src/main/java/com/nicolasgabriel/CadastroDeGravadora/CadastroDeGravadoraApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasgabriel.CadastroDeGravadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeGravadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeGravadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolasgabriel/CadastroDeGravadora/CadastroDeGravadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasgabriel.CadastroDeGravadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeGravadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
